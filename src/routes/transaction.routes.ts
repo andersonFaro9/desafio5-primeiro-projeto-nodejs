745294705c554f7d2c0438f1bb9022631e2efca4
Anderson <faro.anderson@gmail.com>
2020-07-04 21:00:43 -0300

    desafio fundamentos do Node.js with success

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
-
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import { uuid } from 'uuidv4';
+import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,6 +19,17 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
+    const { title, value, type } = request.body;
+    const createTransactions = new CreateTransactionService(
+      transactionsRepository,
+    );
+    const transaction = createTransactions.execute({
+      title,
+      value,
+      type,
+    });
+    return response.json(transaction);
+
     // TODO
   } catch (err) {
     return response.status(400).json({ error: err.message });
